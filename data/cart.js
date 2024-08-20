9fb9a4ff4a3b6c0a4e9cfa74229966a4e9aaf20f
zohaibxJackie <zkhan52607@gmail.com>
2024-08-20 23:39:14 +0500

    add and remove buttons added

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,8 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+  cart = [
+    // The item in the cart will come here after push function
+  ];
 }
 
 function saveToStorage() {
@@ -51,7 +45,13 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
+export function incrementQuantity() {
+  console.log('hello');
+}
 
+export function decrementQuantity() {
+  console.log('hello 2');
+}
 
 export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
--- kashifscripts/checkout.js
+++ kashifscripts/checkout.js
@@ -1,8 +1,26 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-
+import { incrementQuantity, decrementQuantity } from "../data/cart.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
+
+function increaseQuantity() {
+    incrementQuantity()
+}
+
+function decreaseQuantity() {
+    decrementQuantity()
+}
+
+document.querySelectorAll('.quantity-buttons button').forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (event.target.closest('svg').getAttribute('width') === '12' && event.target.closest('svg').getAttribute('height') === '4') {
+            decreaseQuantity();
+        } else {
+            increaseQuantity();
+        }
+    });
+});
